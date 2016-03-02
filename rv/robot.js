582aafeeb68acd570f0cc2be4a7aa8b148df5803
francisgoba <francisgoba@gmail.com>
2016-03-02 11:16:00 -0600

    Create robot.js

---
+++ rv/robot.js
@@ -0,0 +1,36 @@
+function setup() {
+  
+  var esferaForma = new THREE.SphereGeometry(2,50,50);
+  
+  var esfera1 = new THREE.Mesh(esferaForma);
+
+  var forma = new THREE.Geometry();
+  
+  THREE.GeometryUtils.merge(forma, esfera1);
+  
+  malla = new THREE.Mesh( forma );
+  
+  escena = new THREE.Scene();
+  escena.add( malla );
+  
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 10;
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerHeight*.95, window.innerHeight*.95);
+  document.body.appendChild( renderer.domElement );
+}
+
+function loop() {
+  requestAnimationFrame( loop );
+  
+  malla.rotation.x += 0.01;
+  malla.rotation.y += 0.01;
+  
+  renderer.render( escena, camara);
+}
+
+var escena, camara, render, malla;
+
+setup();
+loop();
