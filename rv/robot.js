813a6aac7f11456a7470f86d9cf59b4e80aa1d0b
francisgoba <francisgoba@gmail.com>
2016-03-03 23:54:58 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -7,34 +7,35 @@ function setup() {
       puntos.push(new THREE.Vector2(0.95,2));
   var revo = new THREE.LatheGeometry(puntos,100); 
   var cilindroForma = new THREE.CylinderGeometry(0.02,0.02,0.8,100);
+  var cilindroForma = new THREE.CylinderGeometry(0.02,0.02,0.8,100);
   
   materialcilindro = new THREE.MeshBasicMaterial( { color: 0xFFFFFF } );
       
   var esfera1 = new THREE.Mesh(esferaForma);
   var mediaesfera1 = new THREE.Mesh(mediaesferaForma);
   var revomalla = new THREE.Mesh(revo);
+  cilindromalla = new THREE.Mesh( cilindroForma, materialcilindro );
   
+  cilindromalla.position.y=3.75;
+  cilindromalla.position.x=0.25;
   mediaesfera1.position.y=2.35;
 
   var forma = new THREE.Geometry();
   
-  //THREE.GeometryUtils.merge(forma, esfera1);
+  THREE.GeometryUtils.merge(forma, esfera1);
   THREE.GeometryUtils.merge(forma, mediaesfera1);
   THREE.GeometryUtils.merge(forma, revomalla);
+  THREE.GeometryUtils.merge(forma, cilindromalla);
   
   material = new THREE.MeshBasicMaterial( { color: 0x777777 } );
   
-  cilindromalla = new THREE.Mesh( cilindroForma, materialcilindro );
   malla = new THREE.Mesh( forma, material );
 
   //malla.rotation.x +=Math.PI/2;
   //cilindromalla.rotation.x +=Math.PI/2;
-  cilindromalla.position.y=3.75;
-  cilindromalla.position.x=0.25;
   
   escena = new THREE.Scene();
   escena.add( malla );
-  escena.add( cilindromalla );
   
   camara = new THREE.PerspectiveCamera();
   camara.position.z = 10;
