76828403fe42c7a934bcf92ea44a2723309c99bc
francisgoba <francisgoba@gmail.com>
2016-03-03 22:43:52 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -10,7 +10,7 @@ function setup() {
   var mediaesfera2 = new THREE.Mesh(mediaesferaForma2);
   
   mediaesfera1.position.y=2.5;
-  mediaesfera2.position.y=1.25;
+  mediaesfera2.position.y=1;
   mediaesfera2.rotation.x=Math.PI;
 
   var forma = new THREE.Geometry();
