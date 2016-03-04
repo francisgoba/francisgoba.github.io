8446ed91f845359e005a9b83391013bfd01438cc
francisgoba <francisgoba@gmail.com>
2016-03-03 23:47:48 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -46,8 +46,8 @@ function setup() {
 function loop() {
   requestAnimationFrame( loop );
   
-  malla.rotation.x += 0.01;
-  cilindromalla.rotation.x +=0.01;
+  //malla.rotation.x += 0.01;
+  //cilindromalla.rotation.x +=0.01;
   //malla.rotation.y += 0.01;
   
   renderer.render( escena, camara);
