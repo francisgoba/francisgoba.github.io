67ec5e5ff71bc42fc1e8e19abe61c1414477c647
francisgoba <francisgoba@gmail.com>
2016-03-04 14:06:31 -0600

    Update ejemplo.1.js

--- rv/ejemplo.1.js
+++ rv/ejemplo.1.js
@@ -5,8 +5,8 @@ function Pierna(){
   this.pierna.position.y=-2.5;
   this.pie.position.y=-4.5;
   this.pie.position.x=1;
-  this.add(pierna);
-  this.add(pie);
+  this.add(this.pierna);
+  this.add(this.pie);
 }
 
 Pierna.prototype = new THREE.Object3D();
