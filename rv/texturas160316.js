794d2ffae300f4f7b0a8ed72ba8dfb0571a2a0b1
francisgoba <francisgoba@gmail.com>
2016-03-16 10:29:23 -0600

    Create texturas160316.js

---
+++ rv/texturas160316.js
@@ -0,0 +1,22 @@
+fuction setup(){
+  THREE.ImageUtils.crossOrigin = '';
+  var textura = THREE.ImageUtils.LoadTexture('http://threejs.org/examples/textures/crate.gif');
+  var material = THREE.MeshBasicMaterial({map: textura});
+  var forma = THREE.BoxGeometry(1,1,1);
+  malla =THREE.Mesh(forma, material);
+  escena = new THREE.scene();
+  escena.add(malla);
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 5;
+  
+  renderer = new THREE.WEbGLRenderer();
+  renderer.setSize(window.innerHeight*0.95,window.innerWeight*0.95);
+  document.body.appendChild(renderer.domElement);
+}
+function loop(){
+  requestAnimationFrame(loop);
+  malla.rotation.x+=0.01;
+  malla.rotation.y+=0.01;
+}
+setup();
+loop();
